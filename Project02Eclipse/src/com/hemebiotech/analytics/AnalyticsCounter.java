f1b59d93495e741ecb392a854cb038cf185ef34e
MR-BOUBAKOUR <redha.boubakour@gmail.com>
2024-11-07 16:29:07 +0100

    premier test - code fonctionnel mais sans bonnes pratiques

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,63 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+		// BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+		// String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+		ReadSymptomDataFromFile symptoms = new ReadSymptomDataFromFile("symptoms.txt");
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		FileWriter writer = new FileWriter("result.txt");
+
+		List<String> results = symptoms.GetSymptoms();
+		Map<String, Integer> counters = new HashMap<>();
+
+		results.forEach(result -> {
+			counters.put(result, counters.getOrDefault(result, 0) + 1);
+		});
+
+		Map<String, Integer> sortedCounters = new TreeMap<>(counters);
+
+		sortedCounters.forEach((key, value) -> {
+			try {
+				writer.write(key + ": " + value + "\n");
+				System.out.println(key + ": " + value);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
 		writer.close();
+
+		// int headCount = 0; // counts headaches
+		// while (line != null) {
+
+		// System.out.println("symptom from file: " + line);
+		// if (line.equals("headache")) {
+		// headCount++;
+		// System.out.println("number of headaches: " + headCount);
+		// } else if (line.equals("rush")) {
+		// rashCount++;
+		// } else if (line.contains("pupils")) {
+		// pupilCount++;
+		// }
+
+		// line = reader.readLine(); // get another symptom
+		// }
+		// reader.close();
+
+		// next generate output
+		// FileWriter writer = new FileWriter("result.txt");
+		// writer.write("headache: " + headacheCount + "\n");
+		// writer.write("rash: " + rashCount + "\n");
+		// writer.write("dialated pupils: " + pupilCount + "\n");
+		// writer.close();
 	}
 }
